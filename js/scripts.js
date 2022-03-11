59afc361c377b3b2928029f6d421b27e6898fa99
Rachel Kiarie <rachelnnk94@gmail.com>
2022-03-11 23:33:13 +0300

    Add css for the portfolio section and make them more interactive.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,36 @@
 $(document).ready(function(){
-  $("#design").click(function() {
-   $("#design-showing").toggle();
-   $(".hide-design-image").toggle();
+  //Toggle functionality for the design icon
+  $("#design-image").click(function(){
+    $("#design-detail").show();
+    $("#design-image").hide();
+  });
+  $("#design-detail").click(function(){
+    $("#design-image").show();
+    $("#design-detail").hide();
+  });
+  $("#development-image").click(function(){
+    $("#development-detail").show();
+    $("#development-image").hide();
+  });
+  $("#development-detail").click(function(){
+    $("#development-image").show();
+    $("#development-detail").hide();
+  });
+  $("#product-image").click(function(){
+    $("#product-detail").show();
+    $("#product-image").hide();
+  });
+  $("#product-detail").click(function(){
+    $("#product-image").show();
+    $("#product-detail").hide();
+  });
+
+  
+
+  $("#development").click(function(){
+    $("#dev-showing").toggle();
+
+
   })
   
 
