d008a022993e3d12d5bd8436963f48d50e164381
Rachel Kiarie <rachelnnk94@gmail.com>
2022-03-12 15:32:50 +0300

    Add more css and jQuery functions for hover for the portfolio list section.

--- js/scripts.js
+++ js/scripts.js
@@ -25,13 +25,49 @@ $(document).ready(function(){
     $("#product-detail").hide();
   });
 
-  
-
-  $("#development").click(function(){
-    $("#dev-showing").toggle();
-
-
+  // hover over portfolio list images
+  $("#work-image1").mouseover(function(){
+    $("#work-detail1").show();
+  }).mouseout(function(){
+    $("#work-detail1").hide();
   });
+  $("#work-image2").mouseover(function(){
+    $("#work-detail2").show();
+  }).mouseout(function(){
+    $("#work-detail2").hide();
+  });
+  $("#work-image3").mouseover(function(){
+    $("#work-detail3").show();
+  }).mouseout(function(){
+    $("#work-detail3").hide();
+  });
+  $("#work-image4").mouseover(function(){
+    $("#work-detail4").show();
+  }).mouseout(function(){
+    $("#work-detail4").hide();
+  });
+  $("#work-image5").mouseover(function(){
+    $("#work-detail5").show();
+  }).mouseout(function(){
+    $("#work-detail5").hide();
+  });
+  $("#work-image6").mouseover(function(){
+    $("#work-detail6").show();
+  }).mouseout(function(){
+    $("#work-detail6").hide();
+  });
+  $("#work-image7").mouseover(function(){
+    $("#work-detail7").show();
+  }).mouseout(function(){
+    $("#work-detail7").hide();
+  });
+  $("#work-image8").mouseover(function(){
+    $("#work-detail8").show();
+  }).mouseout(function(){
+    $("#work-detail8").hide();
+  });
+
+
 });
 $(document).ready(function(){
 
